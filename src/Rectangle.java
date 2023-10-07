7fa68208e533cc143afd025692b45c02a103c973
gulkaiyr13 <145272966+gulkaiyr13@users.noreply.github.com>
2023-10-07 19:31:49 +0600

    Rectangle.java

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Rectangle {
+public class Rectangle extends Shape{
+    private double width;
+    private double height;
+    public Rectangle(String name, double width, double height) {
+        super(name);
+        this.width = width;
+        this.height = height;
+    }
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+    @Override
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
 }
