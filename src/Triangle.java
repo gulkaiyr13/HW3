d5b50b9e734e2b07ca7169457303ad216eb5c132
gulkaiyr13 <145272966+gulkaiyr13@users.noreply.github.com>
2023-10-07 19:32:52 +0600

    Triangle.java

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Triangle {
+public class Triangle extends Shape{
+    private double side1;
+    private double side2;
+    private double side3;
+    public Triangle(String name, double side1, double side2, double side3) {
+        super(name);
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+    @Override
+    public double getArea() {
+        double s = (side1 + side2 + side3) / 2;
+        return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+    }
+    @Override
+    public double getPerimeter() {
+        return side1 + side2 + side3;
+    }
 }
