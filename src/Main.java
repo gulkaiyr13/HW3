01844d2f907697e2770f201676bc00a4e690373e
gulkaiyr13 <145272966+gulkaiyr13@users.noreply.github.com>
2023-10-07 19:30:24 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,70 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import java.util.Scanner;
+
+class ShapeCalculator {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        while (true) {
+            System.out.println("Select a shape type:");
+            System.out.println("1. Rectangle");
+            System.out.println("2. Circle");
+            System.out.println("3. Triangle");
+            System.out.println("4. Square");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            if (choice == 5) {
+                System.out.println("POKA");
+                break;
+            }
+
+            Shape shape = null;
+            String shapeName = "";
+
+            switch (choice) {
+                case 1:
+                    shapeName = "Rectangle";
+                    System.out.print("Enter the width: ");
+                    double width = scanner.nextDouble();
+                    System.out.print("Enter the height: ");
+                    double height = scanner.nextDouble();
+                    shape = new Rectangle(shapeName, width, height);
+                    break;
+                case 2:
+                    shapeName = "Circle";
+                    System.out.print("Enter the radius: ");
+                    double radius = scanner.nextDouble();
+                    shape = new Circle(shapeName, radius);
+                    break;
+                case 3:
+                    shapeName = "Triangle";
+                    System.out.print("Enter the length of side 1: ");
+                    double side1 = scanner.nextDouble();
+                    System.out.print("Enter the length of side 2: ");
+                    double side2 = scanner.nextDouble();
+                    System.out.print("Enter the length of side 3: ");
+                    double side3 = scanner.nextDouble();
+                    shape = new Triangle(shapeName, side1, side2, side3);
+                    break;
+                case 4:
+                    shapeName = "Square";
+                    System.out.print("Enter the side length: ");
+                    double sideLength = scanner.nextDouble();
+                    shape = new Square(shapeName, sideLength);
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+                    continue;
+            }
+
+            System.out.println("Selected " + shapeName + ":");
+            System.out.println("Area: " + shape.getArea());
+            System.out.println("Perimeter: " + shape.getPerimeter());
         }
+
+        scanner.close();
     }
 }
